24b681b3e35584e0f4292447af8f1dd8625d0ed4
aieckxis <aleckxiskate@gmail.com>
2023-04-22 14:13:56 +0800

    stored even and odd numbers

--- even-odd-from-file-extractor.py
+++ even-odd-from-file-extractor.py
@@ -6,6 +6,8 @@ with open('numbers.txt', 'r') as f:
 # Read every line in the file and turn it into an integer.
     numbers = [int(line.strip()) for line in f.readlines()]
 # In order to hold even and odd numbers, make two empty lists.
+even_numbers = []
+odd_number = []
 # Run over each number in the input list repeatedly.
 # Test if the number is even.
 # Add the number to the list of even numbers if it is even.

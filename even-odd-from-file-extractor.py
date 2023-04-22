2a575dc5d6d0a24f880835cb2fdea0c27c2c7f23
aieckxis <aleckxiskate@gmail.com>
2023-04-22 14:12:28 +0800

    read file and converted to integers

--- even-odd-from-file-extractor.py
+++ even-odd-from-file-extractor.py
@@ -2,7 +2,9 @@
 # BSCpE 1-4
 
 # Run the numbers.txt input file.
+with open('numbers.txt', 'r') as f:
 # Read every line in the file and turn it into an integer.
+    numbers = [int(line.strip()) for line in f.readlines()]
 # In order to hold even and odd numbers, make two empty lists.
 # Run over each number in the input list repeatedly.
 # Test if the number is even.

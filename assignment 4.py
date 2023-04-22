fd1914a09b1e32e22c75e0d5d83f3927d0cf6015
aieckxis <aleckxiskate@gmail.com>
2023-04-22 14:05:38 +0800

    pseudocode

---
+++ assignment 4.py
@@ -0,0 +1,12 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Run the numbers.txt input file.
+# Read every line in the file and turn it into an integer.
+# In order to hold even and odd numbers, make two empty lists.
+# Run over each number in the input list repeatedly.
+# Test if the number is even.
+# Add the number to the list of even numbers if it is even.
+# Add the number to the list of odd numbers if it is odd.
+# Write even integers into the output file "even.txt" after opening it.
+# Write odd numbers to the output file 'odd.txt' after opening it.
